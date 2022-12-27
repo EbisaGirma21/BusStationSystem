d52fa7de02cd4dae53764ee8f7a71fec268f0da3
EbisaGirma21 <ebisagirma41@gmail.com>
2022-12-27 18:55:46 +0300

    admin dashboard commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# IP-Project-Bus-Station-Management-System
+This project is Internet Programming course project; with HTML,CSS,JavaScript,Nodejs the Bootstrap-5 and Express as a frame work
---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+const btnMenu = document.querySelector(".main-menu");
+const menuText = document.querySelector(".of-txt");
+const menuText1 = document.querySelector(".of-txt1");
+const menuText2 = document.querySelector(".of-txt2");
+const menuText3 = document.querySelector(".of-txt3");
+const menuText4 = document.querySelector(".of-txt4");
+const menuText5 = document.querySelector(".of-txt5");
+const menuText6 = document.querySelector(".of-txt6");
+const menuText7 = document.querySelector(".of-txt7");
+const sideBar = document.querySelector(".sidebar-nav");
+const mainOnMenu = document.querySelector(".main-container");
+const navbarOnMenu = document.querySelector(".navbar");
+btnMenu.addEventListener("click", function () {
+  menuText.classList.toggle("hidden");
+  menuText1.classList.toggle("hidden");
+  menuText2.classList.toggle("hidden");
+  menuText3.classList.toggle("hidden");
+  menuText4.classList.toggle("hidden");
+  menuText5.classList.toggle("hidden");
+  menuText6.classList.toggle("hidden");
+  menuText7.classList.toggle("hidden");
+  sideBar.classList.toggle("reduced-size");
+  mainOnMenu.classList.toggle("main-on-menu");
+  navbarOnMenu.classList.toggle("main-on-menu");
+});
