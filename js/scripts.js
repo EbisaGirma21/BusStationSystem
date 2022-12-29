5e81cc9af6e29ea5308e1c32fb0cd6a70e2a8b57
EbisaGirma21 <ebisagirma41@gmail.com>
2022-12-29 08:27:04 +0300

    some customization commit

--- js/scripts.js
+++ js/scripts.js
@@ -24,5 +24,29 @@ btnMenu.addEventListener("click", function () {
   mainOnMenu.classList.toggle("main-on-menu");
   navbarOnMenu.classList.toggle("main-on-menu");
 });
+menuText3.addEventListener("click", function () {
+  menuText3.classList.toggle("active-link");
+  menuText1.classList.toggle("active-link");
+});
 
-console.log(document.querySelector('.city-name').textContent)
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
